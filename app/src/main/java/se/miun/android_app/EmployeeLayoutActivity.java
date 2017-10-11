670f215f3c4d9523daef25e49d8f4b870cb2d50e
antonflodin93 <anfl1201@student.miun.se>
2017-10-11 12:15:05 +0200

    Started working with layout for the employee part of the app.

---
+++ app/src/main/java/se/miun/android_app/EmployeeLayoutActivity.java
@@ -0,0 +1,14 @@
+package se.miun.android_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class EmployeeLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee_layout);
+    }
+}
