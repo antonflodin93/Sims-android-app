c0809a5f17ab7714795bc5d3fd285254aff1fde8
namlaks <jonas.skalman@gmail.com>
2017-10-17 20:51:44 +0200

    BLE logger feature update 2

---
+++ app/src/main/java/se/miun/android_app/testing/BleScanCallback.java
@@ -0,0 +1,49 @@
+package se.miun.android_app.testing;
+
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanResult;
+import android.util.Log;
+
+import java.util.List;
+import java.util.Map;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created by jony1 on 2017-10-17.
+ */
+
+public class BleScanCallback extends ScanCallback {
+
+    private Map<String, BluetoothDevice> mScanResults;
+
+    BleScanCallback(Map<String, BluetoothDevice> scanResults){
+        mScanResults = scanResults;
+    }
+
+    @Override
+    public void onScanResult(int callbackType, ScanResult result) {
+        addScanResult(result);
+        //super.onScanResult(callbackType, result);
+    }
+
+    @Override
+    public void onBatchScanResults(List<ScanResult> results){
+        for(ScanResult result: results){
+            addScanResult(result);
+        }
+    }
+
+    @Override
+    public void onScanFailed(int errorcode){
+        Log.e(TAG, "BLE Scan Failed with Error code: " + errorcode);
+    }
+
+    public void addScanResult(ScanResult result){
+        BluetoothDevice device = result.getDevice();
+        String deviceAddress = device.getName();
+        mScanResults.put(deviceAddress, device);
+    }
+
+}
--- app/src/main/java/se/miun/android_app/testing/BluetoothLogger.java
+++ app/src/main/java/se/miun/android_app/testing/BluetoothLogger.java
@@ -1,7 +1,13 @@
 package se.miun.android_app.testing;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,13 +16,32 @@ import android.widget.Button;
 import android.content.Intent;
 
 //import se.miun.sims.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import se.miun.android_app.R;
 
 public class BluetoothLogger extends AppCompatActivity implements View.OnClickListener {
+    //ui variables
     private Button bleScan, bleOnOff;
+
+    //Bluetooth Variables
     private BluetoothAdapter mBluetoothAdapter;
-    private int REQUEST_ENABLE_BT = 1;  /*variable needed to be passed into startActivityForResult
-    which must be > 0 ... */
+    private int REQUEST_ENABLE_BT = 1;
+    //scanning variables and objects
+    private HashMap mScanResults;
+    List<ScanFilter> filters;
+    private boolean mScanning;
+    private BluetoothLeScanner mBluetoothLeScanner;
+    private ScanCallback mScanCallback;
+    private ScanSettings settings;
+    private ScanFilter scanFilter;
+    //Scan filter Settings (Scan only for certain device name & address)
+    private String deviceName = "Iggesund SIMS";
+    private String deviceAddress = "e20a39f4-73f5-4bc4-a12f-17d1ad07a961";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,30 +54,93 @@ public class BluetoothLogger extends AppCompatActivity implements View.OnClickLi
         bleOnOff = (Button) findViewById(R.id.bleOnOffButton);
         bleOnOff.setOnClickListener(this);
 
-        //init bluetooth
+        //init bluetooth manager
         final BluetoothManager bluetoothManager =
                 (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        //access android bluetooth object
         mBluetoothAdapter = bluetoothManager.getAdapter();
     }
 
     @Override
     public void onClick(View v){
+        //toggle scan for bluetooth low energy adv...
         if( v.getId() == R.id.bleScanButton ){
-            //Start bluetooth scan for beacon advertisements
-
+            if(mScanning && bluetoothEnable()){
+                stopScan();
+            }
+            else if (bluetoothEnable()){
+                startScan();
+            }
         }
         else if( v.getId() == R.id.bleOnOffButton ){
-            // Ensures Bluetooth is available on the device and it is enabled. If not,
-            // displays a dialog requesting user permission to enable Bluetooth.
-            if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
-                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            //toggle bluetooth on/off
+            if (!bluetoothEnable()) {
+                requestBluetoothEnable();
             }
-            else if (mBluetoothAdapter.isEnabled()){
-                //turn off bluetooth
-                mBluetoothAdapter.disable();
+            else if (bluetoothEnable()){
+                requestBluetoothDisable();
             }
 
         }
+    }
+
+    //start scanning for bluetooth low energy advertisements
+    private void startScan(){
+        //check if scanner is already scanning
+        if(mScanning){
+            return;
+        }
+
+        //build scansettings with filters for device...
+        settings = new ScanSettings.Builder()
+                .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
+                .build();
+
+        //create filter list
+        filters = new ArrayList<>();
+        scanFilter = new ScanFilter.Builder()
+                /*.setDeviceName(deviceName)
+                .setDeviceAddress(deviceAddress)
+                */.build();
+        filters.add(scanFilter); //currently breaks the program...
+
+        //create scanner object
+        mBluetoothLeScanner = mBluetoothAdapter.getBluetoothLeScanner();
+        //create callback
+        mScanResults = new HashMap<>();
+        mScanCallback = new BleScanCallback(mScanResults);
+        //start the scan
+        mBluetoothLeScanner.startScan(filters, settings, mScanCallback);
+        //set scan check enable
+        mScanning = true;
+    }
+
+    //stop scanning for ble devices
+    private void stopScan(){
+        if(mScanning && mBluetoothAdapter != null && mBluetoothAdapter.isEnabled() && mBluetoothLeScanner != null) {
+            mBluetoothLeScanner.stopScan(mScanCallback);
+        }
+        mScanCallback = null;
+        mScanning = false;
+    }
+
+    //check if bluetooth is enabled or disabled
+    private boolean bluetoothEnable(){
+        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
+            return false;
+        }
+        return true;
+    }
+
+    //request enable / turn on bluetooth
+    private void requestBluetoothEnable(){
+        // displays a dialog requesting user permission to enable Bluetooth.
+        Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+    }
+
+    //disable bluetooth
+    private void requestBluetoothDisable(){
+        mBluetoothAdapter.disable();
     }
 }
