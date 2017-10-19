74853789f22f9c8e9a21f5159daaaf869b428134
antonflodin93 <anfl1201@student.miun.se>
2017-10-19 17:50:11 +0200

    Added service activity to test Services in android

---
+++ app/src/main/java/se/miun/android_app/testing/ServiceTestActivity.java
@@ -0,0 +1,15 @@
+package se.miun.android_app.testing;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import se.miun.android_app.R;
+
+public class ServiceTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service_test);
+    }
+}
