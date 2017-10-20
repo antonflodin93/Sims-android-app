166dcdba55b8ffe53017bc66047445b59bc151ba
namlaks <jonas.skalman@gmail.com>
2017-10-20 19:47:10 +0200

    BLE logger, reading RSSI values and device IDs on Scan update

--- app/src/main/java/se/miun/android_app/testing/BleScanCallback.java
+++ app/src/main/java/se/miun/android_app/testing/BleScanCallback.java
@@ -1,9 +1,10 @@
 package se.miun.android_app.testing;
 
-import android.bluetooth.BluetoothDevice;
 import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanResult;
 import android.util.Log;
+import android.widget.TextView;
+
 import java.util.List;
 import java.util.Map;
 
@@ -15,19 +16,30 @@ import static android.content.ContentValues.TAG;
 
 public class BleScanCallback extends ScanCallback {
 
-
-
+    int timeKeeper = 0;
+    private TextView displayTextView;
     private Map<String, ScanResult> mScanResults;
 
-    BleScanCallback(Map<String, ScanResult> scanResults){
+    //constructor
+    BleScanCallback(Map<String, ScanResult> scanResults, TextView displayText){
+        displayTextView = displayText;
         mScanResults = scanResults;
     }
 
+    //function gets called everytime a scan is successful
     @Override
     public void onScanResult(int callbackType, ScanResult result) {
+        /* Test of live update of rssi values */
+//        int rssi = result.getRssi();
+//        String deviceAddress = result.getDevice().getAddress();
+//        displayTextView.append("\nDevice address: " + deviceAddress);
+//        displayTextView.append("\tRSSI: " + rssi);
+        /*end live update rssi test*/
+
         addScanResult(result);
     }
 
+    //store batch results
     @Override
     public void onBatchScanResults(List<ScanResult> results){
         for(ScanResult result: results){
@@ -35,15 +47,17 @@ public class BleScanCallback extends ScanCallback {
         }
     }
 
+    //scanning fails
     @Override
     public void onScanFailed(int errorcode){
         Log.e(TAG, "BLE Scan Failed with Error code: " + errorcode);
     }
 
+    //Store batch result (1 value for each device)
     public void addScanResult(ScanResult result){
-        BluetoothDevice device = result.getDevice();
-        String deviceAddress = device.getName();
-        //int rssi = result.getRssi();
+        //BluetoothDevice device = result.getDevice();
+        String deviceAddress = result.getDevice().getName();
         mScanResults.put(deviceAddress, result);
     }
+
 }
--- app/src/main/java/se/miun/android_app/testing/BluetoothLogger.java
+++ app/src/main/java/se/miun/android_app/testing/BluetoothLogger.java
@@ -5,11 +5,13 @@ import android.bluetooth.BluetoothManager;
 import android.bluetooth.le.BluetoothLeScanner;
 import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanRecord;
 import android.bluetooth.le.ScanResult;
 import android.bluetooth.le.ScanSettings;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.SparseArray;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
@@ -23,6 +25,8 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import se.miun.android_app.R;
 
@@ -76,22 +80,31 @@ public class BluetoothLogger extends AppCompatActivity implements View.OnClickLi
         if( v.getId() == R.id.bleScanButton ){
             if(mScanning && bluetoothEnable()){
                 Toast.makeText(getApplicationContext(), "Stopping Scan", Toast.LENGTH_SHORT).show();
+
                 stopScan();
-                //display results...
+
+                //display batch results with scanComplete();
                 scanComplete();
             }
             else if (bluetoothEnable()){
+
                 Toast.makeText(getApplicationContext(), "Starting Scan", Toast.LENGTH_SHORT).show();
+
+                //timeMe(500);
                 startScan();
             }
         }
         else if( v.getId() == R.id.bleOnOffButton ){
             //toggle bluetooth on/off
             if (!bluetoothEnable()) {
+
                 requestBluetoothEnable();
+
             }
             else if (bluetoothEnable()){
+
                 requestBluetoothDisable();
+
             }
         }
     }
@@ -114,13 +127,13 @@ public class BluetoothLogger extends AppCompatActivity implements View.OnClickLi
                 /*.setDeviceName(deviceName)
                 .setDeviceAddress(deviceAddress)
                 */.build();
-        filters.add(scanFilter); //currently breaks the program...
+        filters.add(scanFilter); //currently breaks the program if filter on name/address
 
         //create scanner object
         mBluetoothLeScanner = mBluetoothAdapter.getBluetoothLeScanner();
         //create callback
         mScanResults = new HashMap<>();
-        mScanCallback = new BleScanCallback(mScanResults);
+        mScanCallback = new BleScanCallback(mScanResults, displayDataTextView);
         //start the scan
         mBluetoothLeScanner.startScan(filters, settings, mScanCallback);
         //set scan check enable
@@ -140,10 +153,45 @@ public class BluetoothLogger extends AppCompatActivity implements View.OnClickLi
             ScanResult results = (ScanResult) mScanResults.get(key);
             String deviceAddress = results.getDevice().getAddress();
             int rssi = results.getRssi();
+            ScanRecord scanRecord = results.getScanRecord();
 
-            //display id and rssi value(range: -127 - 126 dbm )
-            displayDataTextView.append("\nDevice address: " + deviceAddress);
+            int contents = results.describeContents();
+            int txPower = scanRecord.getTxPowerLevel();
+            int advFlags = scanRecord.getAdvertiseFlags();
+            byte[] payload = scanRecord.getBytes();
+            //data we want...
+            //byte[] mData = scanRecord.getManufacturerSpecificData(512);
+
+            long timestampNanos = results.getTimestampNanos();
+
+            displayDataTextView.append("\nname: " + results.getDevice().getName());
+            displayDataTextView.append("\nDeviceAddress: " + deviceAddress);
             displayDataTextView.append("\tRSSI: " + rssi );
+            displayDataTextView.append("\tdist: " + calculateDistance(-56, rssi));
+
+            //displayDataTextView.append("\tTime: " + timestampNanos);
+            displayDataTextView.append("\tTx: " + txPower);
+            //displayDataTextView.append("\tintFlags: " +advFlags);
+            //displayDataTextView.append("\tContents: " + contents);
+
+            displayDataTextView.append("\tmData: ");
+            SparseArray<byte[]> manufacturerData = scanRecord.getManufacturerSpecificData();
+            for(int u = 0; u < manufacturerData.size() ; u++){
+                int manufacturerId = manufacturerData.keyAt(u);
+                displayDataTextView.append("" + manufacturerId);
+            }
+
+            //get manufacturer data
+//            displayDataTextView.append("\tmData: ");
+//            for(int j=0; j < mData.length; j++){
+//                displayDataTextView.append("" + mData[j] );
+//            }
+
+//            displayDataTextView.append("\tPayloadData: ");
+//            for(int j=0; j < payload.length; j++){
+//                displayDataTextView.append("" + payload[j] );
+//            }
+
         }
     }
 
@@ -175,4 +223,68 @@ public class BluetoothLogger extends AppCompatActivity implements View.OnClickLi
     private void requestBluetoothDisable(){
         mBluetoothAdapter.disable();
     }
+
+//    //from https://www.pubnub.com/blog/2015-04-15-build-android-beacon-ibeacon-detector/
+//    private void setScanFilter() {
+//        ScanFilter.Builder mBuilder = new ScanFilter.Builder();
+//        ByteBuffer mManufacturerData = ByteBuffer.allocate(23);
+//        ByteBuffer mManufacturerDataMask = ByteBuffer.allocate(24);
+//        byte[] uuid = getIdAsByte(UUID.fromString("0CF052C297CA407C84F8B62AAC4E9020");
+//        mManufacturerData.put(0, (byte)0xBE);
+//        mManufacturerData.put(1, (byte)0xAC);
+//        for (int i=2; i<=17; i++) {
+//            mManufacturerData.put(i, uuid[i-2]);
+//        }
+//        for (int i=0; i<=17; i++) {
+//            mManufacturerDataMask.put((byte)0x01);
+//        }
+//        mBuilder.setManufacturerData(224, mManufacturerData.array(), mManufacturerDataMask.array());
+//        mScanFilter = mBuilder.build();
+//    }
+//
+//    //from https://www.pubnub.com/blog/2015-04-15-build-android-beacon-ibeacon-detector/
+//    private void setScanSettings() {
+//        ScanSettings.Builder mBuilder = new ScanSettings.Builder();
+//        mBuilder.setReportDelay(0);
+//        mBuilder.setScanMode(ScanSettings.SCAN_MODE_LOW_POWER);
+//        mScanSettings = mBuilder.build();
+//    }
+
+    //algorithm used for distance calculation by Altbeacon.org
+    public double calculateDistance(int txPower, double rssi) {
+        if (rssi == 0) {
+            return -1.0; // if we cannot determine accuracy, return -1.
+        }
+        double ratio = rssi*1.0/txPower;
+        if (ratio < 1.0) {
+            return Math.pow(ratio,10);
+        }
+        else {
+            double accuracy =  (0.89976)*Math.pow(ratio,7.7095) + 0.111;
+            return accuracy;
+        }
+    }
+
+    //timer function, if called, performs the run() function at the specified time (milliSecondDuration)
+    public void timeMe(int milliSecondDuration) {
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            private final int MAX_TIME = 10;
+            int counter = 0;
+
+            @Override
+            public void run() {
+                if (counter < MAX_TIME) {
+                    startScan();
+                    counter++;
+                } else {
+                    cancel();
+                    //displayDataTextView.append("counter is now: " + counter);
+                    stopScan();
+                }
+            }
+        };
+        timer.schedule(task,0,milliSecondDuration);
+    }
+
 }
