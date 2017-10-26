d347d8571a699d4a64abcbae1d9bf39b3e8de90a
antonflodin93 <anfl1201@student.miun.se>
2017-10-26 14:08:02 +0200

    Added master unit layout and button for sending messages

--- app/src/main/java/se/miun/android_app/MasterUnit/MasterUnitActivity.java
+++ app/src/main/java/se/miun/android_app/MasterUnit/MasterUnitActivity.java
@@ -1,15 +1,37 @@
 package se.miun.android_app.MasterUnit;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import se.miun.android_app.EmployeeUnit.EmployeeUnitActivity;
 import se.miun.android_app.R;
 
-public class MasterUnitActivity extends AppCompatActivity {
+public class MasterUnitActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button sendMessagesBtn;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_master_unit);
+        context = this;
+
+        // Init the components
+        sendMessagesBtn = (Button) findViewById(R.id.sendMessagesBtn);
+        sendMessagesBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.sendMessagesBtn){
+            Intent myIntent = new Intent(getApplicationContext(), SendMessageActivity.class);
+            context.startActivity(myIntent);
+        }
+
     }
 }
---
+++ app/src/main/java/se/miun/android_app/MasterUnit/SendMessageActivity.java
@@ -0,0 +1,15 @@
+package se.miun.android_app.MasterUnit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import se.miun.android_app.R;
+
+public class SendMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_message);
+    }
+}
