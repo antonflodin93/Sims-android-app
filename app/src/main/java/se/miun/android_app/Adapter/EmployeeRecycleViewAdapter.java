47f1ea627848c887db551b421ecf3a232340c398
antonflodin93 <anfl1201@student.miun.se>
2017-11-09 14:37:51 +0100

    Added onClick listener for each employee in list

--- app/src/main/java/se/miun/android_app/Adapter/EmployeeRecycleViewAdapter.java
+++ app/src/main/java/se/miun/android_app/Adapter/EmployeeRecycleViewAdapter.java
@@ -1,7 +1,5 @@
 package se.miun.android_app.Adapter;
 
-
-import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,22 +17,37 @@ import java.util.Locale;
 import se.miun.android_app.Model.Employee;
 import se.miun.android_app.R;
 import se.miun.android_app.Model.Message;
+import android.view.View.OnClickListener;
 
 public class EmployeeRecycleViewAdapter extends RecyclerView.Adapter<EmployeeRecycleViewAdapter.EmployeeViewHolder> implements Filterable {
 
-    // Stores the messages
     private List<Employee> employees = new ArrayList<>();
     private EmployeeFilter employeeFilter;
+    private RecyclerView recyclerView;
 
 
     public EmployeeRecycleViewAdapter(List<Employee> employees) {
         this.employees = employees;
+
     }
 
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+        this.recyclerView = recyclerView;
+    }
 
     @Override
     public EmployeeViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.employee_list_row_item, parent, false);
+        view.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int itemPosition = recyclerView.getChildLayoutPosition(v);
+                Employee employee = employees.get(itemPosition);
+                Toast.makeText(v.getContext(), employee.getEmployeeFirstName(), Toast.LENGTH_LONG).show();
+            }
+        });
         return new EmployeeViewHolder(view);
     }
 
@@ -53,7 +66,7 @@ public class EmployeeRecycleViewAdapter extends RecyclerView.Adapter<EmployeeRec
 
 
 
-    public class EmployeeViewHolder extends RecyclerView.ViewHolder {
+    public class EmployeeViewHolder extends RecyclerView.ViewHolder{
         TextView employeeNameTextView, employeeCompanyTextView;
 
         public EmployeeViewHolder(View itemView) {
@@ -62,8 +75,6 @@ public class EmployeeRecycleViewAdapter extends RecyclerView.Adapter<EmployeeRec
             // Init the fields for each row
             employeeNameTextView = (TextView) itemView.findViewById(R.id.employeeNameTextView);
             employeeCompanyTextView = (TextView) itemView.findViewById(R.id.employeeCompanyTextView);
-
-
         }
     }
 
