e335e385ef5bb270d39615f0331549884ecd94f7
antonflodin93 <anfl1201@student.miun.se>
2017-12-01 13:26:42 +0100

    Changed the layout for welcomescreen

--- app/src/main/java/se/miun/android_app/WelcomeActivity.java
+++ app/src/main/java/se/miun/android_app/WelcomeActivity.java
@@ -2,17 +2,31 @@ package se.miun.android_app;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class WelcomeActivity extends Activity {
     private static int SPLASH_TIME = 3000;
+    private ImageView welcomeImageview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        welcomeImageview = (ImageView) findViewById(R.id.welcomeImageview);
+
+
+
+        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+            welcomeImageview.setScaleType(ImageView.ScaleType.FIT_CENTER);
+        } else{
+            welcomeImageview.setScaleType(ImageView.ScaleType.FIT_XY);
+        }
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
